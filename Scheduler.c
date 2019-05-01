ec0089735706f9772377422cfc8602954bf0cf2a
Andrew Sievers <andrew.sievers@selu.edu>
2019-05-01 01:46:45 -0500

    Round Robin now fully working

--- Scheduler.c
+++ Scheduler.c
@@ -5,6 +5,7 @@
 
 void firstComeFirstServe(int ProcessId[], int Arrival[], int cpuBurst[], int Priority[]);
 void shortestJobFirst(int ProcessId[], int Arrival[], int cpuBurst[], int Priority[]);
+void roundRobin(int ProcessId[], int Arrival[], int cpuBurst[], int Priority[]);
 
 	struct Process{
 		int ProcessId;
@@ -23,6 +24,21 @@ void shortestJobFirst(int ProcessId[], int Arrival[], int cpuBurst[], int Priori
 		int completion;
 	};
 
+		//redefine process
+	struct Job{
+		int ProcessId;
+		int Arrival;
+		int cpuBurst;
+		int Priority;
+		bool completed;
+		bool hasArrived;
+		int startTime;
+		int completionTime;
+		int started;
+		int timeleft;
+		int count;
+	};
+
 int main()
 {
 
@@ -36,6 +52,7 @@ int main()
 
 	firstComeFirstServe(ProcessId, Arrival, cpuBurst, Priority);
 	shortestJobFirst(ProcessId, Arrival, cpuBurst, Priority);
+    roundRobin(ProcessId, Arrival, cpuBurst, Priority);
 	return 0;
 }
 
@@ -159,14 +176,106 @@ void shortestJobFirst(int ProcessId[], int Arrival[], int cpuBurst[], int Priori
 	printf("Throughput: %.2f\n", throughput);
 }
 
+void roundRobin(int ProcessId[], int Arrival[], int cpuBurst[], int Priority[]){
+	double throughput, att, art;
+	int runtime, counter, shortestRuntimeIndex;
+	int currentTime = 0;
+	int i = 0;
+	int length = 7;
+	int quanta = 15;
+	int currentJobIndex = 0;
+	bool allProcessesRun = false;
+    struct Job job[7];
 
-/*	
-struct Process{
-		int ProcessId;
-		int Arrival;
-		int cpuBurst;
-		int Priority;
-		bool hasRun;
-		bool hasArrived;
-		};
-*/
+	//  Assign initial structure properties
+	for(i = 0; i < length; i++){
+		job[i].ProcessId = ProcessId[i];
+		job[i].Arrival = Arrival[i];
+		job[i].cpuBurst = cpuBurst[i];
+		job[i].Priority = Priority[i];
+		job[i].completed = false;
+		job[i].hasArrived = false;
+		job[i].timeleft = cpuBurst[i];
+		job[i].count  = 0;
+		job[i].startTime = -1;
+	}
+
+	// Print Header
+	printf("ROUND ROBIN\n");
+	printf("pid\tarrival\tcompletion\n");
+
+	currentJobIndex = 0;
+	while((!allProcessesRun)){
+		//  Find job to run
+		for(i = 0; i < length; i++){
+			if(job[i].count  < job[currentJobIndex].count) {
+				if((!job[i].completed) && (job[i].hasArrived))
+				currentJobIndex = i;
+			}
+		}
+
+		//set start time if not run before
+		if(job[currentJobIndex].startTime = -1){
+			job[currentJobIndex].startTime = currentTime;
+		}
+
+
+		//  Set runtime to quanta OR .timeleft (whichever is lower)
+		if(job[currentJobIndex].timeleft < quanta){
+			runtime = job[currentJobIndex].timeleft;
+		}
+		else runtime = quanta;
+
+		//  Reduce job.timeleft
+		//  If timeleft == 0 
+		//  	1. it marks it as completed
+		//		2. set completionTime
+		//      3. print detail line
+		job[currentJobIndex].timeleft -= runtime;
+		job[currentJobIndex].count += 1;
+		if(job[currentJobIndex].timeleft == 0){
+			job[currentJobIndex].completed = true;
+			job[currentJobIndex].completionTime = currentTime + runtime;
+			job[currentJobIndex].count = 9999999;
+			printf("%d\t%d\t%d\n", job[currentJobIndex].ProcessId, job[currentJobIndex].Arrival, job[currentJobIndex].completionTime);
+		}
+
+		//  Update time
+		currentTime += runtime;
+
+		//  Update hasArrived
+		for(i = 0; i < length; i++){
+			if(job[i].Arrival <= currentTime){
+				job[i].hasArrived = true;
+			}
+		}
+
+		//check if all jobs completed
+		// if all jobs not completed, it sets allProcessesRun to false
+		allProcessesRun = true;
+		for(i = 0; i < length; i++){
+			if(!job[i].completed)
+			allProcessesRun = false;
+		}
+	}
+
+	// print stats
+		//Calc throughput
+	throughput = currentTime / 7.0;
+
+	//Calculate ATT
+	for(i = 0; i < length; i++){
+		att += job[i].completionTime - job[i].Arrival;
+	}
+	att = att / 7.0;
+	printf("Turnaround time: %.2f\n", att);
+
+	// Calculate art
+	for(i = 0; i < length; i++){
+		art += job[i].completionTime - job[i].startTime;
+	}
+	art = art / (double)length;
+	printf("Average respones time = %.2f\n", art);
+	printf("Throughput: %.2f\n\n", throughput);
+}
+
